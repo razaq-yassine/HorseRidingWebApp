b4d3e0f2a6216e5acac6aea393e833f436856065
touhama-ahmed <totouhama@gmail.com>
2021-05-11 18:51:21 +0000

    test

--- app/User.php
+++ app/User.php
@@ -3,9 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use \Illuminate\Contracts\Auth\Authenticatable;
+use Throwable;
 
-class User extends Model implements \Illuminate\Contracts\Auth\Authenticatable
+class User extends Model
 {
     protected $guarded = [];
     protected $primaryKey = 'id_User';
@@ -57,64 +57,4 @@ class User extends Model implements \Illuminate\Contracts\Auth\Authenticatable
         //returns array of Admins
     }
 
-    /**
-     * Get the name of the unique identifier for the user.
-     *
-     * @return string
-     */
-    public function getAuthIdentifierName()
-    {
-        // TODO: Implement getAuthIdentifierName() method.
-    }
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
-    public function getAuthIdentifier()
-    {
-        // TODO: Implement getAuthIdentifier() method.
-    }
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
-    public function getAuthPassword()
-    {
-        // TODO: Implement getAuthPassword() method.
-    }
-
-    /**
-     * Get the token value for the "remember me" session.
-     *
-     * @return string
-     */
-    public function getRememberToken()
-    {
-        // TODO: Implement getRememberToken() method.
-    }
-
-    /**
-     * Set the token value for the "remember me" session.
-     *
-     * @param string $value
-     * @return void
-     */
-    public function setRememberToken($value)
-    {
-        // TODO: Implement setRememberToken() method.
-    }
-
-    /**
-     * Get the column name for the "remember me" token.
-     *
-     * @return string
-     */
-    public function getRememberTokenName()
-    {
-        // TODO: Implement getRememberTokenName() method.
-    }
 }
