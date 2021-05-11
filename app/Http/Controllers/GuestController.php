692cb945e4b4ccf82ff5890d60731e36b7858c55
touhama-ahmed <totouhama@gmail.com>
2021-05-11 18:42:13 +0000

    test

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -12,18 +12,18 @@ use Illuminate\Support\Facades\Auth;
 class GuestController extends Controller
 {
     public function login(request $request){
-//        request()->validate([
-//            'email' => 'required|email|exists:users,Email_User',
-//            'password' => 'required',
-//        ],[
-//            'email.required' => "Désole l'adresse mail est obligatoire",
-//            'email.email' => "Désole le format de l'adresse mail est invalide.",
-//            'email.exists' => "Désole, adresse électronique ou mot de passe non valide.",
-//            'password.required' => "Désole le mot de passe est obligatoire",
-//        ]);
+        request()->validate([
+            'email' => 'required|email|exists:users,Email_User',
+            'password' => 'required',
+        ],[
+            'email.required' => "Désole l'adresse mail est obligatoire",
+            'email.email' => "Désole le format de l'adresse mail est invalide.",
+            'email.exists' => "Désole, adresse électronique ou mot de passe non valide.",
+            'password.required' => "Désole le mot de passe est obligatoire",
+        ]);
 
         // cheking credentials
-        if(Auth::attempt(['Email_User' => $request->header("email"), 'password' => bcrypt($request->header("password"))])){
+        if(Auth::attempt(['Email_User' => request('email'), 'password' => request('password')])){
             return response()->json([
                 'Success' => "true",
             ]);
@@ -31,8 +31,6 @@ class GuestController extends Controller
         else{
             return response()->json([
                 'Success' => "false",
-                "email" => $request->header("email"),
-                "password" => $request->header("password"),
             ]);
 //            return back()->withErrors([
 //                'email' => 'Désole, adresse électronique ou mot de passe non valide.',
